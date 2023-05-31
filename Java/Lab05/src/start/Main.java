99c7f7b437e5bf14136eb50ea8e2576a314658b5
bohanbtw <mybuisnessbohdan@gmail.com>
2023-05-31 03:36:48 +0300

    Lab05 added cases

--- Java/Lab05/src/start/Main.java
+++ Java/Lab05/src/start/Main.java
@@ -1,24 +1,45 @@
 package start;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Task 1");
-        Task1 t1 = new Task1();
-        t1.Init();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose task [1-2-3-4]");
+        int number = scanner.nextInt();
+
+        switch (number){
+            case 1:
+                System.out.println("Task 1");
+                Task1 t1 = new Task1();
+                t1.Init();
+                break;
+            case 2:
+                System.out.println("Task 2");
+                Task2 t2 = new Task2();
+                t2.Init();
+                break;
+            case 3:
+                System.out.println("Task 3");
+                Task3 t3 = new Task3();
+                t3.Init();
+                break;
+            case 4:
+                System.out.println("Task 4");
+                Task4 t4 = new Task4();
+                t4.Init();  
+                break;
+            default:
+                System.out.println("Wrong number");
+                break;       
+        }
+        
 
 
-        System.out.println("Task 2");
-        Task2 t2 = new Task2();
-        t2.Init();
 
 
-        System.out.println("Task 3");
-        Task3 t3 = new Task3();
-        t3.Init();
+        
 
 
-        System.out.println("Task 4");
-        Task4 t4 = new Task4();
-        t4.Init();
+       
     }
 }
