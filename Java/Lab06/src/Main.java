bf6d0a47adb7bf15125fcb5b3ea8619446c9c780
bohanbtw <mybuisnessbohdan@gmail.com>
2023-05-31 03:39:08 +0300

    Added cases Lab05

--- Java/Lab06/src/Main.java
+++ Java/Lab06/src/Main.java
@@ -5,9 +5,18 @@ import figures.Shape;
 import figures.SolidOfRevolution;
 import figures.Task3;
 import library.LibraryCard;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose task [1-2-3-4]");
+        int number = scanner.nextInt();
+
+        switch (number){
+            case 1:
+            
+        }
         System.out.print("Task 1");
         Person person = new Person("Bohdan",19);
         person.Move();
