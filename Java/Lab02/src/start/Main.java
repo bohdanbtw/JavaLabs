844fe7ab48c4451c13d1a75cdd7c85efbdbd7b26
bohanbtw <mybuisnessbohdan@gmail.com>
2023-05-31 03:19:26 +0300

    Cases Lab02

--- Java/Lab02/src/start/Main.java
+++ Java/Lab02/src/start/Main.java
@@ -1,29 +1,40 @@
 package start;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] arrays){
-        System.out.println("Task 1");
-        Calculator calc = new Calculator();
-        calc.Launch();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Choose task [1-2-3-4-5]");
+        int number = scanner.nextInt();
 
-        System.out.println("Task 2");
-        CheckNumbers cn = new CheckNumbers();
-        cn.Launch();
-
-
-        System.out.println("Task 3");
-        FindX fx = new FindX();
-        fx.Launch();
-
-
-        System.out.println("Task 4");
-        Quadrant qd = new Quadrant();
-        qd.Launch();
-
-
-        System.out.println("Task 5");
-        QuadraticEquation qe = new QuadraticEquation();
-        qe.Launch();
+        switch (number){
+            case 1:
+                System.out.println("Task 1");
+                Calculator calc = new Calculator();
+                calc.Launch();
+                break;
+            case 2:
+                System.out.println("Task 2");
+                CheckNumbers cn = new CheckNumbers();
+                cn.Launch();
+                break;
+            case 3:
+                System.out.println("Task 3");
+                FindX fx = new FindX();
+                fx.Launch();
+                break;
+            case 4:
+                System.out.println("Task 4");
+                Quadrant qd = new Quadrant();
+                qd.Launch();
+                break;
+            case 5:
+                System.out.println("Task 5");
+                QuadraticEquation qe = new QuadraticEquation();
+                qe.Launch();
+                break;
+        }
+    
     }
 }
