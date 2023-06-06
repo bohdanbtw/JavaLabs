16a49d2c430d5ee5d0ca0ff9d7565e12d7532189
bohdanbtw <mybuisnessbohdan@gmail.com>
2023-06-06 18:50:45 +0300

    Оновлено код

--- Java/Lab05/src/start/Task2.java
+++ Java/Lab05/src/start/Task2.java
@@ -10,9 +10,9 @@ public class Task2 {
     }
 
     public void GetTheSmallestNumber(){
-        System.out.println("The smalllest number is: " + CalculateSMallestNumber());
+        System.out.println("The smalllest number is: " + CalculateSMallestNumber(first,second,third));
     }
-    private int CalculateSMallestNumber(){
+    private int CalculateSMallestNumber(int first, int second, int third){
         if(first < second && first < third){
             return first;
         }
